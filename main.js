a9719e181398833822812ff08200d07b30bf7acd
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-08-14 12:14:54 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+let todoList = []
+var input = document.getElementById('input')
+var todoDisplay = document.getElementById('listDisplay')
+
+function addTask(){
+    if(input.value === ''){
+        console.err('No Text');
+    }
+    else{
+        var newTodo = {todo: input.value, id: Math.floor(Math.random() * 100)}
+        todoList = [...todoList, newTodo];
+        displayTask();
+        input.value = '';
+    }
+   
+}
+
+function displayTask(){
+    listDisplay.innerHTML = ''
+    for(let i =0; i < todoList.length; i++){
+        let todoLi =document.createElement('li')
+        todoLi.innerHTML = todoList[i].todo
+        todoLi.id = todoList[i].id
+        
+        let deleteButton = document.createElement('button')
+        deleteButton.onclick = deleteTodo
+        deleteButton.innerHTML = `Delete`
+        todoLi.appendChild(deleteButton)
+
+        let editButton = document.createElement('button')
+        editButton.onclick = editTodo
+        editButton.innerHTML = `Edit`
+        todoLi.appendChild(editButton)
+
+        todoDisplay.appendChild(todoLi)
+    }
+}
+
+
+function deleteTodo(){
+    console.log('delete')
+}
+
+function editTodo(){
+    console.log('edit')
+}
